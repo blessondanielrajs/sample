9474f73b043499a2726899650722a9e38f7f0327
John Blesswin A <johnblesswina@Johns-MacBook-Pro.local>
2022-02-01 20:10:55 +0530

    Updates

---
+++ precache-manifest.64f9475f0353f53832290be3a4e82b59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0209eaab9ca581bdc4939a1d79e3039",
+    "url": "/sample%20/index.html"
+  },
+  {
+    "revision": "4a478a49f62d0dc9e045",
+    "url": "/sample%20/static/css/main.bc821aff.chunk.css"
+  },
+  {
+    "revision": "99efb8c2989eed87a800",
+    "url": "/sample%20/static/js/2.ebf60a8a.chunk.js"
+  },
+  {
+    "revision": "4ce21b8e6f6ceee2c592483ff5da69f2",
+    "url": "/sample%20/static/js/2.ebf60a8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a478a49f62d0dc9e045",
+    "url": "/sample%20/static/js/main.478cb13b.chunk.js"
+  },
+  {
+    "revision": "00818be3de7f157b7254",
+    "url": "/sample%20/static/js/runtime-main.fe4814a6.js"
+  }
+]);
